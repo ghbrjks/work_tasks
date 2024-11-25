c863ab07af1bdb653b6a75e9b5e20507554f62e3
ghbrjks <shadow.pixel47@gmail.com>
2024-11-25 23:48:18 +0300

    task1

--- task1/students.php
+++ task1/students.php
@@ -4,22 +4,22 @@ require_once("students_logic.php");
 <div class="container">
     <div class="row">
         <div class="col-12">
-            <table class="table mt-3">
+            <table class="table mt-3 col">
                 <thead>
                 <tr>
                     <th scope="col">  </th>
-                    <?php foreach($data as $item): ?>
-                    <th scope="col"><?=htmlspecialchars($item[1])?></th>
+                    <?php foreach($subjects as $subject): ?>
+                    <th scope="col"><?=htmlspecialchars($subject)?></th>
                     <?php endforeach; ?>
                 </tr>
                 </thead>
                 <tbody>
-                <?php foreach($data as $item):?>
+                <?php foreach($students as $student):?>
                     <tr>
-                        <td><?=htmlspecialchars($item['0'])?></td>
-                        <td><?=htmlspecialchars($item['2'])?></td>
-                        <td><?=htmlspecialchars($item['2'])?></td>
-                        <td><?=htmlspecialchars($item['2'])?></td>
+                        <td><?=htmlspecialchars($student)?></td>
+                        <?php foreach($subjects as $subject): ?>
+                            <td></td>
+                        <?php endforeach; ?>
                     </tr>
                 <?php endforeach;?>
                 </tbody>
--- task1/students_logic.php
+++ task1/students_logic.php
@@ -8,10 +8,14 @@ $data = [
     ['Иванов', 'Физика', 4],
     ['Петров', 'ОБЖ', 4],
 ];
-array_multisort($data);
+$subjects = array_unique(array_column($data, 1));
+$students = array_unique(array_column($data, 0));
+
 for ($i = 0; $i < count($data)-1; $i++) {
     if($data[$i][0] === $data[$i+1][0] && $data[$i][1] === $data[$i+1][1]) {
         $data[$i+1][2] = $data[$i+1][2] + $data[$i][2];
         unset($data[$i]);
     }
 }
+sort($subjects);
+sort($students);
