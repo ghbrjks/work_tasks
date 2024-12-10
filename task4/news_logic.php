81c0ec0c27849f8355e07b28f27a81f12c26c014
ghbrjks <shadow.pixel47@gmail.com>
2024-12-11 01:07:42 +0300

    fixed

--- task4/news_logic.php
+++ task4/news_logic.php
@@ -11,45 +11,61 @@ $text = <<<TXT
             <i><b>Великая Отечественная война в истории города</b></i></p><p><i>Важнейшей операцией Советской Армии в Великой Отечественной войне стала <a href="https://www.calend.ru/holidays/0/0/1869/">Сталинградская битва</a> (17.07.1942 - 02.02.1943). Целью боевых действий советских войск являлись оборона  Сталинграда и разгром действовавшей на сталинградском направлении группировки противника. Победа советских войск в Сталинградской битве имела решающее значение для победы Советского Союза в Великой Отечественной войне.</i>
         </p>
 TXT;
-function get_clickbait($text){
+function get_clickbait($text) {
     $dom = new DOMDocument();
     @$dom->loadHTML(mb_convert_encoding($text, 'HTML-ENTITIES', 'UTF-8'));
     $words_counter = 0;
-    $res = '';
     $words_limit = 29;
+    $result_dom = new DOMDocument();
 
-    foreach($dom->getElementsByTagName('p') as $node){
-        if ($words_counter >= $words_limit){ break; }
-
-        $node_html = $dom->saveHTML($node);
-        $node_text = strip_tags($node_html);
-        // разделение слов по пробелам и тире
-        $words = preg_split("/[\s\-]+/u", $node_text, -1, PREG_SPLIT_NO_EMPTY);
-        $words_capacity = $words_limit - $words_counter;
-
-        if(count($words) <= $words_capacity){
-            // если слов меньше лимита добавляется весь узел
-            $res .= $node_html;
-            $words_counter += count($words);
+    foreach ($dom->getElementsByTagName('p') as $node) {
+        if ($words_counter >= $words_limit) {
+            break;
         }
-        else{
-            // если слов больше лимита, то лишние слова обрезаются
-            $trimmed_words = array_slice($words, 0, $words_capacity);
-            // добавление многоточия к последнему слову
-            $last_word = array_pop($trimmed_words);
-            $last_word .= '...';
-            $trimmed_words[] = $last_word;
-            $trimmed_text = implode(' ', $trimmed_words);
 
-            // формирование нового узла
-            $new_node = $dom->createElement('p');
-            $new_node->setAttribute('class', $node->getAttribute('class'));
-            $new_node->nodeValue = $trimmed_text;
+        // копирование узла
+        $node_clone = $result_dom->importNode($node, true);
+        $words_counter = processNode($node_clone, $words_counter, $words_limit);
 
-            $res .= $dom->saveHTML($new_node);
-            $words_counter += $words_capacity;
+        $result_dom->appendChild($node_clone);
+
+        if ($words_counter >= $words_limit) {
             break;
         }
     }
-    return $res;
+
+    return $result_dom->saveHTML();
 }
+
+function processNode($node, $words_counter, $words_limit) {
+    if ($node->nodeType === XML_TEXT_NODE) {
+        $node_text = $node->nodeValue;
+        $words = preg_split('/[\s\-]+/u', $node_text, -1, PREG_SPLIT_NO_EMPTY);
+        $remaining_words = $words_limit - $words_counter;
+
+        if (count($words) > $remaining_words) {
+            // если слов больше лимита, то лишние слова обрезаются
+            $trimmed_words = array_slice($words, 0, $remaining_words);
+            $node->nodeValue = implode(' ', $trimmed_words) . '...';
+            $words_counter = $words_limit;
+        } else {
+            // если слов меньше лимита добавляется весь текст
+            $words_counter += count($words);
+        }
+    } elseif ($node->hasChildNodes()) {
+        // обработка дочерних узлов
+        foreach ($node->childNodes as $child) {
+            if ($words_counter >= $words_limit) {
+                // удаление узлов, если достигнут лимит
+                while ($node->lastChild !== $child) {
+                    $node->removeChild($node->lastChild);
+                }
+                break;
+            }
+            $words_counter = processNode($child, $words_counter, $words_limit);
+        }
+    }
+
+    return $words_counter;
+}
+
